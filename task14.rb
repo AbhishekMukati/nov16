ceafd0c717091e9aa57ad27fc7f34f6f7fc89dc0
developer <you@example.com>
2022-11-16 18:55:45 +0530

    aaj 16

---
+++ task14.rb
@@ -0,0 +1,8 @@
+a = [{"id": 1 , "name": "ram"},{"id": 3 , "name": "sita" }] 
+     b = {"id": 3, "age": 24 } 
+       key = "age" 
+          val = 24 
+              d = a[1].store(key , val) 
+                 puts a[0]
+                   puts a[1] 
+ # fb;gfbn
